61101576e991a4117b96b97539c88f3339dc0a83
Priyanka K <priyanka.sony@gmail.com>
2025-07-18 14:42:44 +0530

    Fix error

--- src/app/get-involved/volunteer-opportunities/page.js
+++ src/app/get-involved/volunteer-opportunities/page.js
@@ -1,11 +1,11 @@
 'use client'
 import React, { Fragment } from 'react';
 import NavbarS3 from '@/components/NavbarS3/NavbarS3'
-import PageTitle from '../../../components/pagetitle/PageTitle'
-import Volunteer from '../../../components/Volunteer/Volunteer';
-import CtaSection from '../../../components/CtaSection/CtaSection';
-import Footer from '../../../components/footer/Footer';
-import Scrollbar from '../../../components/scrollbar/scrollbar';
+import PageTitle from '@/components/pagetitle/PageTitle'
+import Volunteer from '@/components/Volunteer/Volunteer';
+import CtaSection from '@/components/CtaSection/CtaSection';
+import Footer from '@/components/footer/Footer';
+import Scrollbar from '@/components/scrollbar/scrollbar';
 
 import Logo from '/public/images/sight_care_logo.png'
 
