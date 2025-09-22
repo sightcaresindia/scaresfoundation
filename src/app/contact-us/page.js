ed483002fab6b1b790c76c7109184f093787f825
Priyanka K <priyanka.sony@gmail.com>
2025-09-22 13:34:00 +0530

    Remove Unnessary Files

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -3,10 +3,6 @@ import React, { Fragment } from 'react';
 import NavbarS3 from '@/components/NavbarS3/NavbarS3'
 import PageTitle from '@/components/pagetitle/PageTitle'
 import Contactpage from '@/components/Contactpage/Contactpage';
-import MissionVision from '@/components/MissionVision/MissionVision';
-import Story from '@/components/Story/Story';
-import Leadership from '@/components/Leadership/Leadership';
-import CtaSection from '@/components/CtaSection/CtaSection';
 import Footer from '@/components/footer/Footer';
 import Scrollbar from '@/components/scrollbar/scrollbar';
 
